155b047a55249dd985c32c8fbc790a94f2597740
Christina Yi <89492759+cdyi51@users.noreply.github.com>
2024-09-09 16:58:42 -0400

    Update PeckishPhilosopher.java

--- javaThreads 2/implementation/PeckishPhilosopher.java
+++ javaThreads 2/implementation/PeckishPhilosopher.java
@@ -1,129 +1,132 @@
-package implementation;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.Semaphore;
 import java.util.Random;
 
-public class PeckishPhilosopher implements Runnable{
-
-    int order;
-    boolean isEating;
-    boolean isThinking;
+class PeckishPhilosopher implements Runnable {
+    //semaphore to allow only 2 philosophers to eat at the same time
     private static final Semaphore semaphore = new Semaphore(2);
-    private static final int numPhilos = 5;
 
-    private static int firstEatingPhilo = 0;
-    private static int secondEatingPhilo = 2;
+    private static final int numPhilosophers = 5;
+
+    //random number generator for random sleep intervals
+    private static final Random random = new Random();
+
+    private static int firstPhilosopher = 0;
+    private static int secondPhilosopher = 2;
 
     private static int rounds;
 
+    private static int firstEatingTime = 0;
+    private static int secondEatingTime = 0;
+
+    //variables to track when both philosophers are done eating
+    private static boolean firstPhilosopherDone = false;
+    private static boolean secondPhilosopherDone = false;
+
     private final int philosopherId;
+    
+    private static final Object roundLock = new Object();
 
+    public PeckishPhilosopher(int id) {
+        this.philosopherId = id;
+    }
 
-            public PeckishPhilosopher(int id) {
-                // id to distinguish philos from each other
-                this.philosopherId = id;
-                //this.order = counter.getAndIncrement()
-            }
-
-            public boolean change(Philosopher next){
-
-                synchronized (this) {
-                    synchronized (next){
-                        if (this.order < next.order) {
-                            this.isEating = false;
-                            this.isThinking = true;
-                            next.isEating = true;
-                            next.isThinking = false;
-                            return true;
-                        }
-                        return false;
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < rounds; i++) {
+                synchronized (roundLock) {
+                    //philosophers eat if they are selected
+                    if (canEat(philosopherId)) {
+                        //acquire semaphore to make sure only 2 philosophers eat at a time
+                        semaphore.acquire();
+                        eat(i + 1);
+                        semaphore.release();
                     }
                 }
-
+                think(i + 1);
             }
-            public synchronized boolean getIsEating() { return isEating; }
-            public synchronized boolean getIsThinking() { return isThinking; }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
+    //check if the philosopher is allowed to eat
+    private boolean canEat(int philosopherId) {
+        return philosopherId == firstPhilosopher || philosopherId == secondPhilosopher;
+    }
 
-            public void run(){
-                try{
-                    for(int i =0; i< rounds; i++){
-                        synchronized (PeckishPhilosopher.class){
-                            //philos eat if they are selected
-                            if(canEat(philosopherId)){
-                                //checks that you are one of the 2 assigned philos that can eat
-                                //secondary check w semaphore (there are 2)
-                                semaphore.acquire();
-                                eat(i+1);//eating for randomized
-                                semaphore.release();
-                            }
-                        }
-                        think(i+1);
-                    }
-                } catch (InterruptedException e){
-                    Thread.currentThread().interrupt();
-                }
-            }
-
-
-            private void eat(int round) throws InterruptedException{
-                int eatingTime = random.nextInt(3000) + 1000;
-                System.out.println("Round " +  round + ": Philosopher "+ philosopherId + "is eating for " + eatingTime );
-                Thread.sleep(eatingTime);
-            }
-
-
-
-
-        final Philosopher phil1 =  new Philosopher("A");
-        phil1.isEating = true;
-        phil1.isThinking = false;
-        final Philosopher phil2 =  new Philosopher("B");
-        phil2.isEating = false;
-        phil2.isThinking = true;
-        final Philosopher phil3 =  new Philosopher("C");
-        phil3.isEating = true;
-        phil3.isThinking = false;
-        final Philosopher phil4 =  new Philosopher("D");
-        phil4.isEating = false;
-        phil4.isThinking = true;
-        final Philosopher phil5 =  new Philosopher("E");
-        phil5.isEating = false;
-        phil5.isThinking = true;
-
-        class RoundOne extends Thread {
-            public void run(){
-                for (int i = 0; i < 100000; i++) {
-                    phil1.change(phil2);
-                }
-            }
+    private void eat(int round) throws InterruptedException {
+        int eatingTime = random.nextInt(1000) + 1000;
+        if (philosopherId == firstPhilosopher) {
+            firstEatingTime = eatingTime;
+        } else if (philosopherId == secondPhilosopher) {
+            secondEatingTime = eatingTime;
         }
 
-        class RoundTwo extends Thread {
-            public void run(){
-                for (int i = 0; i < 100000; i++) {
-                    phil3.change(phil4);
-                }
+        System.out.println("Round " + round + ": Philosopher " + philosopherId + " is eating for " + eatingTime + " milliseconds.");
+
+        Thread.sleep(eatingTime);
+
+        // synchronize the two philosophers' eating times
+        synchronized (roundLock) {
+            if (philosopherId == firstPhilosopher) {
+                firstPhilosopherDone = true;
+            } else if (philosopherId == secondPhilosopher) {
+                secondPhilosopherDone = true;
             }
+
+            //check if both philosophers are done eating, sync
+            checkAndSyncPhilosophers(round);
         }
+    }
 
-        RoundOne thread1 = new RoundOne();
-        RoundTwo thread2 = new RoundTwo();
-        thread1.start(); thread2.start();
-        thread1.join(); thread2.join();
-        System.out.println("phil1 is eating: " + phil1.getIsEating());
-        System.out.println("phil2 is eating: " + phil2.getIsEating());
-        System.out.println("phil3 is eating: " + phil3.getIsEating());
-        System.out.println("phil4 is eating: " + phil4.getIsEating());
-        System.out.println("phil5 is eating: " + phil5.getIsEating());
+    // Simulate philosopher thinking
+    private void think(int round) throws InterruptedException {
+        System.out.println("Round " + round + ": Philosopher " + philosopherId + " is thinking.");
+        Thread.sleep(1000); 
+    }
 
-        System.out.println("phil1 is thinking: " + phil1.getIsThinking());
-        System.out.println("phil2 is thinking: " + phil2.getIsThinking());
-        System.out.println("phil3 is thinking: " + phil3.getIsThinking());
-        System.out.println("phil4 is thinking: " + phil4.getIsThinking());
-        System.out.println("phil5 is thinking: " + phil5.getIsThinking());
+    // Check both philosophers and ensure they move to the next round only when both are done eating
+    private static void checkAndSyncPhilosophers(int round) throws InterruptedException {
+        //check if both philosophers are done eating
+        if (firstPhilosopherDone && secondPhilosopherDone) {
+            int longerEatingTime = Math.max(firstEatingTime, secondEatingTime);
+            int shorterEatingTime = Math.min(firstEatingTime, secondEatingTime);
 
+            //the philosopher with the shorter eating time should wait for the difference
+            int timeDifference = longerEatingTime - shorterEatingTime;
 
+            System.out.println("Philosopher with the shorter eating time waits for " + timeDifference + " milliseconds.");
+            Thread.sleep(timeDifference); 
+            //reset done status for the next round
+            firstPhilosopherDone = false;
+            secondPhilosopherDone = false;
 
+            System.out.println("Both philosophers have finished Round " + round + ".");
 
+            randomSleepBeforeNextRound();
+
+            shiftPhilosophers();
+        }
+    }
+
+    //shift the two eating philosophers to the right
+    private static void shiftPhilosophers() {
+        firstPhilosopher = (firstPhilosopher + 1) % numPhilosophers;
+        secondPhilosopher = (firstPhilosopher + 2) % numPhilosophers;
+
+        System.out.println("Next philosophers to eat: " + firstPhilosopher + " and " + secondPhilosopher);
+    }
+
+    //sleep for a random amount of time before the next round starts
+    private static void randomSleepBeforeNextRound() throws InterruptedException {
+        int sleepTime = random.nextInt(3000) + 1000; 
+        System.out.println("Sleeping for " + sleepTime + " milliseconds before next round.");
+        Thread.sleep(sleepTime); 
+    }
+
+    public static void setRounds(int numRounds) {
+        rounds = numRounds;
+    }
+}
 }

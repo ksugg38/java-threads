e1ef730977a55bf40572bd7b4159b347134ab0c7
Christina Yi <89492759+cdyi51@users.noreply.github.com>
2024-09-09 16:52:23 -0400

    Create Demo.java

---
+++ javaThreads 2/Demo.java
@@ -0,0 +1,24 @@
+public class Demo {
+    public static void main(String[] args) {
+        int numRounds = 5; // Set how many times the philosophers should eat and think
+        DiningPhilosophers.setRounds(numRounds);
+
+        // Create 5 philosopher threads
+        Thread[] philosophers = new Thread[5];
+        for (int i = 0; i < 5; i++) {
+            philosophers[i] = new Thread(new DiningPhilosophers(i));
+            philosophers[i].start();
+        }
+
+        // Wait for all threads to finish
+        for (Thread philosopher : philosophers) {
+            try {
+                philosopher.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        System.out.println("All philosophers have finished " + numRounds + " rounds.");
+    }
+}
